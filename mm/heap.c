a83d071731da23b276e4608ecd40c4f450e71c7e
Luke Nooteboom <l.nooteboom@gmail.com>
2016-11-11 06:26:48 +0100

    Finished kernel logical memory manager

--- mm/heap.c
+++ mm/heap.c
@@ -23,7 +23,6 @@ static inline memArea_t *getFooterFromHeader(memArea_t *header) {
 
 void *kmalloc(size_t size) {
 	//basic first-fit mm
-	asm("xchgw %bx, %bx");
 	if (size & 7) {
 		size &= 7;
 		size += 8;
@@ -32,10 +31,8 @@ void *kmalloc(size_t size) {
 
 	memArea_t *newHeader = heapStart;
 	bool foundMem = false;
-	while ((uintptr_t)(newHeader) < (uintptr_t)(heapStart) + HEAPSIZE){
-		sprint("newHeader: ");
-		hexprintln(newHeader);
-		if (!(*newHeader & 1) && *newHeader >= totalSize) {
+	while ( (uintptr_t)(newHeader) < (uintptr_t)(heapStart) + HEAPSIZE){
+		if ( !(*newHeader & 1) && *newHeader >= totalSize) {
 			foundMem = true;
 			break;
 		} else {
@@ -50,10 +47,9 @@ void *kmalloc(size_t size) {
 	memArea_t *freeFooter = getFooterFromHeader(newHeader);
 	*newHeader = totalSize;
 	memArea_t *newFooter = getFooterFromHeader(newHeader);
-	asm("xchgw %bx, %bx");
 
 	if (totalSize != oldSize) {
-		//create new header for other area
+		//split and create new header for other area
 		memArea_t *freeHeader = (void*)(newHeader) + *newHeader;
 		*freeHeader = oldSize - totalSize;
 		*freeFooter = *freeHeader;
@@ -66,7 +62,7 @@ void *kmalloc(size_t size) {
 }
 
 void kfree(void *addr) {
-	if (addr >= (void*)(heapStart) && addr < ((void*)(heapStart) + HEAPSIZE)) {
+	if (addr >= (void*)(heapStart) && addr < ( (void*)(heapStart) + HEAPSIZE)) {
 		memArea_t *header = (addr - sizeof(memArea_t));
 		if (!(*header & 1)) {
 			//already freed
@@ -79,20 +75,20 @@ void kfree(void *addr) {
 		memArea_t *nextHeader = (void*)(header) + *header;
 		if (nextHeader < (heapStart + HEAPSIZE) && !(*nextHeader & 1)) {
 			//merge higher
-			memArea_t *nextFooter = ((void*)(nextHeader) + *nextHeader - sizeof(memArea_t));
+			memArea_t *nextFooter = ( (void*)(nextHeader) + *nextHeader - sizeof(memArea_t));
 			*header += *nextHeader;
 			*nextFooter = *header;
 			footer = nextFooter;
 		} else {
-			footer = ((void*)(header) + *header - sizeof(memArea_t));
+			footer = ( (void*)(header) + *header - sizeof(memArea_t));
 		}
 		*footer &= ~1;
 
 		memArea_t *prevFooter = header - 1;
 		memArea_t *prevHeader = (void*)(prevFooter) - *prevFooter + sizeof(memArea_t);
-		if (prevFooter > heapStart && !( (uintptr_t)(prevFooter) & 1) ) { //if previous area was free
+		if (prevFooter > heapStart && !( (uintptr_t)(prevFooter) & 1) ) { //if previous area is free
 			//merge lower
-			*prevHeader += *header; //error prevHeader = BC109D30
+			*prevHeader += *header;
 			*footer = *prevHeader;
 		}
 	}
