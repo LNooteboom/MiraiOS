d69d78c9bc1837f4026f794c1fb8e35400c61879
racing19th <l.nooteboom@gmail.com>
2017-02-09 11:00:04 +0100

    Cleaned up virtpaging

--- arch/x86_64/mm/virtpaging.c
+++ arch/x86_64/mm/virtpaging.c
@@ -11,49 +11,64 @@
 
 spinlock_t KVMemLock;
 
-static bool checkMemRange(uintptr_t vaddr, uint16_t nrofPages) {
+/*
+Checks a memory range starting at vaddr
+returns NULL if available, address of present page if not available
+*/
+static uintptr_t checkMemRange(uintptr_t vaddr, uint16_t nrofPages) {
 	bool ret = true;
+	bool large = false;
 	for (uint16_t i = 0; i < nrofPages; i++) {
 		bool present = true;
-		//hexprint64(vaddr);
 		for (int8_t level = NROF_PAGE_LEVELS - 1; level >= 0; level--) {
 			pte_t pte = *mmGetEntry(vaddr, level);
 			if (!(pte & (PAGE_FLAG_PRESENT | PAGE_FLAG_INUSE))) {
 				present = false;
 				break;
 			} else if (pte & PAGE_FLAG_SIZE) {
+				large = true;
 				break;
 			}
 		}
-		//sprint(": ");
-		//hexprintln(present);
 		if (present) {
 			ret = false;
 			break;
 		}
+		hexprintln64(vaddr);
 		vaddr += PAGE_SIZE;
 	}
-	//hexprintln(ret);
-	return ret;
+	if (!ret) {
+		if (large) {
+			vaddr += LARGEPAGE_SIZE;
+		} else {
+			vaddr += PAGE_SIZE;
+		}
+		return vaddr;
+	} else {
+		return NULL;
+	}
 }
 
 uintptr_t allocKPages(uint16_t nrofPages, physPageFlags_t flags) {
 	uintptr_t vaddr = (uintptr_t)&BSS_END_ADDR & ~(LARGEPAGE_SIZE - 1);
+	vaddr += LARGEPAGE_SIZE;
 	bool foundPages = false;
-	uint16_t nrofKMemPages = (KERNEL_VMEM_END - (uintptr_t)&BSS_END_ADDR) / PAGE_SIZE;
-	uint16_t nrofTests = nrofKMemPages - nrofPages;
+	uintptr_t lastPage = KERNEL_VMEM_END - (nrofPages * PAGE_SIZE);
+
 	acquireSpinlock(&KVMemLock);
-	for (uint16_t i = 0; i < nrofTests; i++) {
-		if (checkMemRange(vaddr, nrofPages)) {
+	while (vaddr <= lastPage) {
+		uintptr_t newAddr = checkMemRange(vaddr, nrofPages);
+		if (!newAddr) {
 			for (uint16_t j = 0; j < nrofPages; j++) {
 				mmReservePage(vaddr + (j * PAGE_SIZE), flags);
 			}
 			foundPages = true;
 			break;
 		}
-		vaddr += PAGE_SIZE;
+		vaddr = newAddr;
 	}
 	releaseSpinlock(&KVMemLock);
+
 	if (foundPages) {
 		return vaddr;
 	} else {
